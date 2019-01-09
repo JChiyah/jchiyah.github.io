06fcdbdfba9a107660870a9bb5676c9148f11f01
jchiyah <jchiyah@outlook.com>
2019-01-09 00:53:30 +0000

    Updates

---
+++ precache-manifest.d3e21eb0552d8a592ed558eec13f977b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "117e2da323b0a4fc0d9e",
+    "url": "/static/js/main.117e2da3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "117e2da323b0a4fc0d9e",
+    "url": "/static/css/main.91434de0.chunk.css"
+  },
+  {
+    "revision": "7c4cf964d86eec820c793ee5ae10c910",
+    "url": "/index.html"
+  }
+];
