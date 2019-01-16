242cf7370711e18da7f3a33b37555e02e8d97ba5
jchiyah <jchiyah@outlook.com>
2019-01-16 23:26:30 +0000

    Add About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -8,7 +8,12 @@ class About extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1> Hello from about!</h1>
+				<NavigationBar currentPage='About' />
+
+				<div className="app-body">
+					<h1>About Me</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
 			</div>
 		);
 	}
