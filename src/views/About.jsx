bb7e6498a3b70fa1fc8f0fd6ef6fe011f3ef78fb
jchiyah <jchiyah@outlook.com>
2019-01-17 21:20:04 +0000

    Add dummy text for incompleted pages

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -12,6 +12,7 @@ class About extends Component {
 
 				<div className="app-body">
 					<h1>About Me</h1>
+					<h2>Work in progress, sorry!</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 			</div>
--- src/views/Activities.jsx
+++ src/views/Activities.jsx
@@ -8,7 +8,13 @@ class Activities extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1> Hello from professional activities!</h1>
+				<NavigationBar currentPage='Activities' />
+
+				<div className="app-body">
+					<h1>Professional Activities</h1>
+					<h2>Work in progress, sorry!</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
 			</div>
 		);
 	}
