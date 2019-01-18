ba8022bec2af807ae71220cb4e16e700654d07e9
jchiyah <jchiyah@outlook.com>
2019-01-18 12:01:54 +0000

    Add NavigationBar to 404 page

--- src/views/PageNotFound.jsx
+++ src/views/PageNotFound.jsx
@@ -9,9 +9,13 @@ class PageNotFound extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>404 Error</h1>
+				<NavigationBar currentPage='' />
 
-				<h2>It seems that this page does not exists</h2>
+				<div className="app-body">
+					<h1>404 Error</h1>
+
+					<h2>It seems that this page does not exists</h2>
+				</div>
 			</div>
 		);
 	}
