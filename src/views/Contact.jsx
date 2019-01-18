0ae373861eff17de01c0d9153451c88057d2ceca
jchiyah <jchiyah@outlook.com>
2019-01-18 12:46:48 +0000

    Add completed contact page

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './../App.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 import NavigationBar from './../components/NavigationBar';
 
 
@@ -10,11 +14,32 @@ class Contact extends Component {
 			<div className="App">
 				<NavigationBar currentPage='Contact' />
 
-				<div className="app-body">
-					<h1>Professional Activities</h1>
-					<h2>Work in progress, sorry!</h2>
-					<p>Contact me at <strong><tt>jchiyah@outlook.com</tt></strong></p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div className="app-body contact">
+					<h1>Contact</h1>
+					<p>The easiest way to contact me is through email, and then we can arrange something else (e.g. Skype, phone or in person). <br/><br/>I might not see messages sent to LinkedIn or through other means.</p>
+					
+					<hr/>
+					<div className="email-box">
+						<h2><FontAwesomeIcon className="fa-icon" icon={faEnvelope} />&nbsp; Email</h2>
+						<p>Preferable email for contact:</p>
+
+						<a href="mailto:jchiyah@outlook.com">jchiyah@outlook.com</a>
+					</div>
+					<hr/>
+					<div className="email-box">
+						<h2><FontAwesomeIcon className="fa-icon" icon={faEnvelope} />&nbsp; Alternative Email</h2>
+						<p>Feel free to contact me at my university email if appropriate. My current university email is below:</p>
+
+						<a href="mailto:jchiyah@outlook.com">fjc3@hw.ac.uk</a><br/><br/>
+						<p>Note that this email may change over time!</p>
+					</div>
+					<hr/>
+					<h2>Other related pages:</h2>
+					<ul>
+						<li><a href="https://github.com/jchiyah"><FontAwesomeIcon className="fa-icon" icon={faGithub} /> GitHub profile</a></li>
+						<li><a href="https://www.linkedin.com/in/javier-chiyah-garcia-469045a6/"><FontAwesomeIcon className="fa-icon" icon={faLinkedin} /> LinkedIn profile</a></li>
+						<li><a href="https://scholar.google.co.uk/citations?hl=en&user=NQyCFjYAAAAJ#"><FontAwesomeIcon className="fa-icon" icon={faGraduationCap} /> Google Scholar</a></li>
+					</ul>
 				</div>
 			</div>
 		);
