e32cfbb36e190d849e48407ae5cfcf4729828cee
jchiyah <jchiyah@outlook.com>
2019-01-18 12:02:17 +0000

    Fix unsafe links in Publications.jsx

--- src/views/Publications.jsx
+++ src/views/Publications.jsx
@@ -51,8 +51,8 @@ class Publications extends Component {
 
 				<div className="app-body">
 					<h1>Publications</h1>
-					<p style={{marginBottom: '2em'}}>The following is a list of publications. You can also check my <a href={publicationsFile} target="_blank"><FontAwesomeIcon className="fa-icon" icon={faBook} /> bibtex file</a> or 
-						my <a href="https://scholar.google.co.uk/citations?hl=en&user=NQyCFjYAAAAJ#" target="_blank"><FontAwesomeIcon className="fa-icon" icon={faGraduationCap} /> Google Scholar profile</a>.
+					<p style={{marginBottom: '2em'}}>The following is a list of publications. You can also check my <a href={publicationsFile} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fa-icon" icon={faBook} /> bibtex file</a> or 
+						my <a href="https://scholar.google.co.uk/citations?hl=en&user=NQyCFjYAAAAJ#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fa-icon" icon={faGraduationCap} /> Google Scholar profile</a>.
 						<br/><br/>If you need access to any of the publications and the link is broken, do not hesitate to contact me and I will happily provide a copy.
 					</p>
 					<ul>{pubs}</ul>
@@ -60,7 +60,7 @@ class Publications extends Component {
 					<hr/>
 					<h2>My Name</h2>
 					<p>Sometimes there is confusion about my name and the correct way to use it. I have two surnames <em>Chiyah</em> and <em>Garcia</em>, thus both "should" be used (but it doesn't matter too much!). 
-					I follow the guidelines from this very <a href="https://blog.apastyle.org/apastyle/2017/05/whats-in-a-name-two-part-surnames-in-apa-style.html" target="_blank">comprenhensive guide</a>. 
+					I follow the guidelines from this very <a href="https://blog.apastyle.org/apastyle/2017/05/whats-in-a-name-two-part-surnames-in-apa-style.html" target="_blank" rel="noopener noreferrer">comprenhensive guide</a>. 
 					Check below for the quick answer.<br/><br/>
 
 					I recommend the following styles, being <em>Chiyah</em> my preferred surname:</p>
