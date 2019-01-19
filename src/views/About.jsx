e2c0e13fbe07dfeb47b894b1398359cf899fd3e8
jchiyah <jchiyah@outlook.com>
2019-01-19 16:41:00 +0000

    Add about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -12,8 +12,31 @@ class About extends Component {
 
 				<div className="app-body">
 					<h1>About Me</h1>
-					<h2>Work in progress, sorry!</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<div className="about">
+						<div className="about-img-wrapper">
+							<img src="about_photo.jpg" alt="selfie" />
+						</div>
+						<p>
+							I am currently a 5th Year student of an MEng in Software Engineering at Heriot-Watt University, Edinburgh (UK).
+							<br/><br/>
+
+							I have worked in a few cool projects during internships with industry that have led to research output. 
+							I have also published a couple of articles from my 4th Year thesis and co-authored a few more from university research projects.
+							<br/><br/>
+
+							Python, PHP, C++, Javascript and Java are my strongest programming languages. Most of my important projects involved one or more of these. 
+							I like to use stable and modern technology stacks. I am also familiar with many frameworks in these languages 
+							(e.g. Vue, React, Laravel, etc.) and other tools (Gulp, CMake, Hunter, etc.). 
+							<br/><br/>
+
+							My interests include human-computer interaction, autonomous systems, natural language (e.g. chatbots) and explainability.
+							<br/><br/>
+
+							I will finish my studies in May 2019 and I am currently looking to do a PhD in one of these areas, particularly something to do 
+							with explainability and autonomy (e.g. control of systems through natural language). Ideally, this PhD would
+							involve applied research. Any offers are welcome!
+						</p>
+					</div>
 				</div>
 			</div>
 		);
