1d03be5997ce42ebdda50d7e7781f4dffaa368ae
jchiyah <jchiyah@outlook.com>
2019-01-23 00:06:05 +0000

    Add PageNotFound style

--- src/views/PageNotFound.jsx
+++ src/views/PageNotFound.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './../App.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSadTear } from '@fortawesome/free-solid-svg-icons';
+
 import NavigationBar from './../components/NavigationBar';
 import Footer from './../components/Footer';
 
@@ -10,12 +13,20 @@ class PageNotFound extends Component {
 	render() {
 		return (
 			<div className="App">
-				<NavigationBar currentPage='' />
+				<NavigationBar currentPage='404 Error' />
 
-				<div className="app-body">
-					<h1>404 Error</h1>
+				<div className="app-body page-not-found">
+					<h1>Ooops!</h1>
 
-					<h2>It seems that this page does not exists</h2>
+					<h2>Page not found</h2>
+
+					<h3>404 Error</h3>
+
+					<FontAwesomeIcon className="fa-icon" icon={faSadTear} style={{fontSize: '5em'}} />
+
+					<h4>It seems that this page does not exists (or I forgot something)</h4>
+
+					<p>Please, send me an email if you think that there should be something here!</p>
 				</div>
 
 				<Footer />
