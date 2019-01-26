bb989059d7ada9b78d70bff3573cbab27b91352b
jchiyah <jchiyah@outlook.com>
2019-01-26 17:12:05 +0000

    Fix typo in about

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -21,11 +21,11 @@ class About extends Component {
 							I am currently a 5th Year student of an MEng in Software Engineering at Heriot-Watt University, Edinburgh (UK).
 							<br/><br/>
 
-							I have worked in a few cool projects during internships with industry that have led to research output. 
+							I have worked in a few cool projects during internships within the industry that have led to research output. 
 							I have also published a couple of articles from my 4th Year thesis and co-authored a few more from university research projects.
 							<br/><br/>
 
-							Python, PHP, C++, Javascript and Java are my strongest programming languages. Most of my important projects involved one or more of these. 
+							Python, PHP, C++, JavaScript and Java are my strongest programming languages. Most of my important projects involved one or more of these. 
 							I like to use stable and modern technology stacks. I am also familiar with many frameworks in these languages 
 							(e.g. Vue, React, Laravel, etc.) and other tools (Gulp, CMake, Hunter, etc.). 
 							<br/><br/>
