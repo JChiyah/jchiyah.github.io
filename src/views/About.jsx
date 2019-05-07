5e583b786493a34ff0187de92e9a3243fe1a8aee
jchiyah <jchiyah@outlook.com>
2019-05-07 15:31:28 +0100

    Update About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -21,8 +21,8 @@ class About extends Component {
 							I am currently a 5th Year student of an MEng in Software Engineering at Heriot-Watt University, Edinburgh (UK).
 							<br/><br/>
 
-							I have worked in a few cool projects during internships within the industry that have led to research output. 
-							I have also published a couple of articles from my 4th Year thesis and co-authored a few more from university research projects.
+							I have worked in a few cool projects during internships within the industry that have led to research output.
+							I have also published a couple of articles from my 4th Year thesis and co-authored some more from university research projects.
 							<br/><br/>
 
 							Python, PHP, C++, JavaScript and Java are my strongest programming languages. Most of my important projects involved one or more of these. 
@@ -33,9 +33,8 @@ class About extends Component {
 							My interests include human-computer interaction, autonomous systems, natural language (e.g. chatbots) and explainability.
 							<br/><br/>
 
-							I will finish my studies in May 2019 and I am currently looking to do a PhD in one of these areas, particularly something to do 
-							with explainability and autonomy (e.g. control of systems through natural language). Ideally, this PhD would
-							involve applied research. Any offers are welcome regardless of location!
+							I finish my MEng studies in May 2019 and I will start an iCASE PhD in September 2019 with Siemens in
+							the field of monitoring and control of systems using natural language.
 						</p>
 					</div>
 				</div>
