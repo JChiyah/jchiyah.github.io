e2a4a1c1c10837e8b0440ea631470a40104c664a
Javier Chiyah-Garcia <jchiyah@outlook.com>
2025-02-25 14:33:53 +0000

    Rework pagenotfound

--- src/views/PageNotFound.jsx
+++ src/views/PageNotFound.jsx
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSadTear } from '@fortawesome/free-solid-svg-icons';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 import PageLayout from './PageLayout';
 
 
-class PageNotFound extends Component {
+const PageNotFound = () => {
+	return (
+		<PageLayout
+			pageTitle="Not Found">
+			<div className="container">
+				<div className="row justify-content-center text-center">
+					<div className="col-md-8 col-lg-6">
+						<div className="border-0 rounded-3 p-4">
+							<div className="card-body">
+								<h1 className="display-1 fw-bold mb-3">404</h1>
+								<br />
 
-	render() {
-		return (
-			<PageLayout>
-				<div className="app-body page-not-found">
-					<h1>Ooops!</h1>
+								<FontAwesomeIcon
+									icon={faCog}
+									className="text-accent mb-4"
+									style={{
+										fontSize: '10rem',
+										animationDuration: '25s'
+									}}
+									spin={true}
+								/>
 
-					<h2>Page not found</h2>
+								<h2 className="mb-3 fw-bold">Oops! Page Not Found</h2>
 
-					<h3>404 Error</h3>
+								<p className="mb-4 text-muted">
+									The page you're looking for doesn't exist or has been moved.
+								</p>
 
-					<FontAwesomeIcon className="fa-icon" icon={faSadTear} style={{ fontSize: '5em' }} />
-
-					<h4>It seems that this page does not exists (or I forgot something)</h4>
-
-					<p>Please, send me an email if you think that there should be something here!</p>
+								<div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+									<Link to="/" className="btn btn-primary btn-lg px-4 gap-2">
+										Return Home
+									</Link>
+									<a href="/#contact" className="btn btn-outline-secondary btn-lg px-4">
+										Contact Me
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</PageLayout>
-		);
-	}
-}
+			</div>
+		</PageLayout>
+	);
+};
 
 export default PageNotFound;
